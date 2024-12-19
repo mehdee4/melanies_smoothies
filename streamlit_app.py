b291a5b557d53f50a0eeaf3ea911aa0ea8955da4
mehdee4 <33725083+mehdee4@users.noreply.github.com>
2024-12-19 12:04:30 +0200

    Update streamlit_app.py
    show nutsisious for chosen fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake. snowpark. functions import col
+import requests
+
 
 # Write directly to the app
 st.title("Customize Your Smoothie!")
@@ -28,7 +30,9 @@ if ingredients_list:
     ingredients_string = '' 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    #st.write(ingredients_string)
 
 
     my_insert_stmt = """ insert into smoothies.public.orders (name_on_order, ingredients)
@@ -43,8 +47,5 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display smoothiefroot nutrition information
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
